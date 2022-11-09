e1eb7fe6be2b4c790bc6ee3150bc397fa5ca92d6
Rajib2027 <110389089+Rajib2027@users.noreply.github.com>
2022-11-09 19:39:48 +0530

    Exception Handling Source Code

---
+++ InvaliduserInput.java
@@ -0,0 +1,10 @@
+package com.te.restaurant;
+
+public class InvaliduserInput extends Exception {
+	
+
+	public InvaliduserInput(String v) {
+		// TODO Auto-generated constructor stub
+		super(v);
+	}
+}
